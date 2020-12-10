affa63d731f5c7374fd1bcd9d3bd831ae769c700
vector-client-token <>
2020-12-10 20:06:58 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -1,9 +1,12 @@
 
 
 @transform_pandas(
-    Output(rid="ri.vector.main.execute.035c0f61-2fcb-4424-aad0-fa2823df0acd"),
-    inpatient_encoded_w_imputation=Input(rid="ri.foundry.main.dataset.d3578a81-014a-49a6-9887-53d296155bdd")
+    Output(rid="ri.foundry.main.dataset.65022b1b-0ea5-4c2f-a577-49a867e3d07e"),
+    inpatient_encoded_w_imputation=Input(rid="ri.foundry.main.dataset.d3578a81-014a-49a6-9887-53d296155bdd"),
+    outcomes=Input(rid="ri.foundry.main.dataset.3d9b1654-3923-484f-8db5-6b38b56e290c")
 )
-def unnamed(inpatient_encoded_w_imputation):
-    
+def data_encoded_and_outcomes(inpatient_encoded_w_imputation, outcomes):
+    i = inpatient_encoded_w_imputation
+    o = outcomes
+    return i.join(o, on=['visit_occurrence_id'], how='inner')
 
