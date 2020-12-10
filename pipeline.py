8be2808fae0318af08988812461fb67fcf982360
vector-client-token <>
2020-12-10 20:25:00 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -1,4 +1,16 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from mpl_toolkits import mplot3d
+import seaborn as sns
+import sklearn.cluster as cluster
+from sklearn.metrics import adjusted_rand_score, adjusted_mutual_info_score, accuracy_score, confusion_matrix, plot_roc_curve, roc_auc_score, roc_curve, auc
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.feature_selection import RFE, RFECV
+from sklearn.pipeline import Pipeline
 
+import xgboost as xgb
 
 @transform_pandas(
     Output(rid="ri.foundry.main.dataset.65022b1b-0ea5-4c2f-a577-49a867e3d07e"),
@@ -9,4 +21,11 @@ def data_encoded_and_outcomes(inpatient_encoded_w_imputation, outcomes):
     i = inpatient_encoded_w_imputation
     o = outcomes
     return i.join(o, on=['visit_occurrence_id'], how='inner')
+
+@transform_pandas(
+    Output(rid="ri.vector.main.execute.d699d42b-7448-42fc-85d3-ada317ac6e46"),
+    data_encoded_and_outcomes=Input(rid="ri.foundry.main.dataset.65022b1b-0ea5-4c2f-a577-49a867e3d07e")
+)
+def unnamed(data_encoded_and_outcomes):
+    
 
